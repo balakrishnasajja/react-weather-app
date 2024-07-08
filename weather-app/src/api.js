a9eda8b6a07a017f4de830b46713816ed98e9a32
BALAKRISHNA S <balakrishna1342001@gmail.com>
2024-07-08 19:13:51 +0530

    weather-app-commit

---
+++ weather-app/src/api.js
@@ -0,0 +1,11 @@
+
+
+export const getApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ec3970be30msha922a4c651c01a5p1f4c4bjsn1968825935ea',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
