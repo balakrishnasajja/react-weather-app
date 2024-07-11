928120cd04f3c21d079cddc69ffc0e01f8933a1e
BALAKRISHNA S <balakrishna1342001@gmail.com>
2024-07-11 21:02:51 +0530

    'weather-app-final'

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -9,3 +9,5 @@ export const getApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "d4e7680c622d5f460046c5242ffb5e0a"
